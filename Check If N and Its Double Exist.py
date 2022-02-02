d3eb7db54f407dd4d693f7f91112b5fc3e2210ac
Deepak Krishnan E <44614318+deeepak-me@users.noreply.github.com>
2022-02-03 00:16:20 +0530

    leetcode Qstn

---
+++ Check If N and Its Double Exist.py
@@ -0,0 +1,15 @@
+Check If N and Its Double Exist
+
+Solution 1:
+
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        
+        for i in range(len(arr)):
+            for j in range(len(arr)):
+                if (arr[i] == arr[j]*2) and i!= j:
+                    return (True)
+            
+                
+     
+      
